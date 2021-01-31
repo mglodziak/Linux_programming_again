daba34adb95a935e7313b40f5ea651f16b775c41
mglodziak <mikolaj0608@gmail.com>
2021-01-31 17:09:54 +0100

    dodane get_args

--- producent_konsument/konsument.c
+++ producent_konsument/konsument.c
@@ -5,25 +5,25 @@
 
 void get_args(int* argc, char** argv[], float *p, int *c, float *d)
 {
-        int opt;
-        while ((opt=getopt(*argc, *argv, "p:c:d:")) != -1)
-        {
-                switch (opt)
-                {
-                        case 'p':
-                                *p=strtof(optarg,NULL);
-                                break;
-			case 'c':
-                                *c=strtol(optarg,NULL,10);
-                                break;
-			case 'd':
-                                *d=strtof(optarg,NULL);
-                                break;
-                        default:
-                                printf("Wrong args!\n");
-                                exit(EXIT_FAILURE);
-                }
-        }
+  int opt;
+  while ((opt=getopt(*argc, *argv, "p:c:d:")) != -1)
+  {
+    switch (opt)
+    {
+      case 'p':
+      *p=strtof(optarg,NULL);
+      break;
+      case 'c':
+      *c=strtol(optarg,NULL,10);
+      break;
+      case 'd':
+      *d=strtof(optarg,NULL);
+      break;
+      default:
+      printf("Wrong args!\n");
+      exit(EXIT_FAILURE);
+    }
+  }
 }
 
 
@@ -31,14 +31,13 @@ void get_args(int* argc, char** argv[], float *p, int *c, float *d)
 
 int main(int argc, char* argv[])
 {
-        float p=0;
-	int c=0;
-	float d=0;
+  float p=0;
+  int c=0;
+  float d=0;
 
-        get_args(&argc, &argv, &p, &c, &d);
-        printf("p=%.3f\n",p);
-	printf("c=%d\n",c);
-	printf("d=%.3f\n",d);
-        return 0;
+  get_args(&argc, &argv, &p, &c, &d);
+  printf("p=%.3f\n",p);
+  printf("c=%d\n",c);
+  printf("d=%.3f\n",d);
+  return 0;
 }
-
--- producent_konsument/producent.c
+++ producent_konsument/producent.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-void get_args(int* argc, char** argv[], float *p)
+void get_args(int* argc, char** argv[], float *p, int *subst_p, int *port, char** adress)
 {
 	int opt;
 	while ((opt=getopt(*argc, *argv, "p:")) != -1)
@@ -11,24 +11,46 @@ void get_args(int* argc, char** argv[], float *p)
 		switch (opt)
 		{
 			case 'p':
-				*p=strtof(optarg,NULL);
-				break;
+			*p=strtof(optarg,NULL);
+			*subst_p=1;
+			break;
 			default:
-				printf("Wrong args!\n");
-				exit(EXIT_FAILURE);
+			printf("Wrong args!\n");
+			exit(EXIT_FAILURE);
 		}
 	}
+
+//*port=strtol((*argv)[optind], NULL, 10);
+sprintf(*adress,"%s\n",(*argv)[optind]);
 }
 
+
+void check_count_args(int* argc)
+{
+	if ((*argc)-optind!=3)
+	{
+		printf("Usage: ./prod -p <float> [adress:]<port> - Exiting...\n");
+		exit( -1);
+	}
+}
 
 
 
 int main(int argc, char* argv[])
 {
 	float p=0;
+	int subst_p=0;
+	int port=0;
 
-	get_args(&argc, &argv, &p);
+	char* adress = (char*)malloc(sizeof(char)*20);
+
+
+  check_count_args(&argc);
+	get_args(&argc, &argv, &p, &subst_p, &port, &adress);
 	printf("P=%.3f\n",p);
+  printf("subst_p=%d\n",subst_p);
+  printf("port=%d\n",port);
+	printf("address=%s\n",adress);
+
 	return 0;
 }
-
